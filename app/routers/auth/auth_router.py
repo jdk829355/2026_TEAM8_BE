925dc484ce16927ca1ea9ef61c2c442c2a2d4427
jdk829355 <jdk829355@gmail.com>
2026-03-30 14:30:59 +0900

    이메일 인증 로직 추가 및 사용자 정보 업데이트 핸들러 구현

--- app/routers/auth/auth_router.py
+++ app/routers/auth/auth_router.py
@@ -32,12 +32,16 @@ def create_user(create_user_request: CreateUserRequest, service: AuthService = D
     return {"message": "사용자가 성공적으로 생성되었습니다."}
 
 @router.get("/send_email")
-def send_email_endpoint(email: str, service: AuthService = Depends(get_auth_service), db=Depends(get_db), user_id: str = Depends(get_current_user_id)):
+def send_email_endpoint(email: str, service: AuthService = Depends(get_auth_service), db=Depends(get_db)):
     logger = logging.getLogger(__name__)
 
     if not service.is_email_duplicated(db=db, email=email):
         logger.error(f"Email does not exist: {email}")
         raise HTTPException(status_code=400, detail="존재하지 않는 이메일입니다.")
+
+    if service.check_email_verification(db=db, email=email):
+        logger.error(f"Email already verified: {email}")
+        raise HTTPException(status_code=400, detail="이미 인증된 이메일입니다.")
     
     token = generate_email_verification_token(email)
 
@@ -75,9 +79,9 @@ def verify_email(token: str, service: AuthService = Depends(get_auth_service), d
     return HTMLResponse(content=html_content, status_code=200)
 
 
-@router.post("/user/register")
-def register_user():
-    return {"message": "register_user handler"}
+@router.patch("/user_info")
+def update_user_info():
+    return {"message": "update_user_info handler"}
 
 
 @router.post("/login", response_model=LoginResponse)
@@ -88,6 +92,9 @@ def login(login_request: LoginRequest, service: AuthService = Depends(get_auth_s
     
     if not service.verify_password(login_request.password, user.password): # type: ignore
         raise HTTPException(status_code=400, detail="잘못된 비밀번호입니다.")
+    
+    if not user.is_verified: # type: ignore
+        raise HTTPException(status_code=400, detail="이메일 인증이 필요합니다.")
     
     return LoginResponse(
         access_token=generate_jwt(user_id=user.id), # type: ignore
